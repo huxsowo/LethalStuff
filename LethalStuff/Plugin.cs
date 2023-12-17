529b66dfa65424df0c07ffd3b89e5a621768db54
huxsowo <trennttpotteerr@gmail.com>
2023-12-17 18:20:34 -0500

    Add project files.

---
+++ LethalStuff/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using BepInEx;
+using LethalSDK;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using BepInEx.Logging;
+
+namespace LethalStuff
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class LethalStuffMain : BaseUnityPlugin
+    {
+        private const string modGUID = "Roadhouse.LethalStuff";
+        private const string modName = "Lethal Stuff";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static LethalStuffMain Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("FEBO IS FATTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT");
+
+            harmony.PatchAll(typeof(LethalStuffMain));
+        }
+    }
+}
