59a438f9f0c147828e43828050efb4ab4ad06dd1
huxsowo <trennttpotteerr@gmail.com>
2023-12-17 19:42:30 -0500

    Added GameState debug messages.

--- LethalStuff/Plugin.cs
+++ LethalStuff/Plugin.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HarmonyLib;
+using LC_API;
 using BepInEx.Logging;
+using LC_API.GameInterfaceAPI;
 
 namespace LethalStuff
 {
@@ -34,6 +36,22 @@ namespace LethalStuff
             mls.LogInfo("FEBO IS FATTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT");
 
             harmony.PatchAll(typeof(LethalStuffMain));
+
+            LC_API.ServerAPI.ModdedServer.SetServerModdedOnly();
+
+            GameState.PlayerDied += GameState_PlayerDied;
+
+            GameState.LandOnMoon += GameState_LandOnMoon;
+        }
+
+        private void GameState_LandOnMoon()
+        {
+            GameTips.ShowTip("nuts", "balls");
+        }
+
+        private void GameState_PlayerDied()
+        {
+            GameTips.ShowTip("nuts", "dead");
         }
     }
 }
